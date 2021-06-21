e8f368d63694e484b82feffc7d0d41bff3fd0280
huanglinjia <1248651631@qq.com>
2021-06-21 14:39:46 +0800

    Update jove.insurance contracts

--- truffle-config.js
+++ truffle-config.js
@@ -21,9 +21,8 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-//const fs = require('fs');
-//const mnemonic = fs.readFileSync(".secret").toString().trim();
-const mnemonic = "victory maze fly also celery element endorse wise seat elevator pact cash";
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -64,7 +63,7 @@ module.exports = {
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-      from:'0xA899F3a037Aae7Ab9601b92c7a6aBD9B22ee33b1',
+      from:'0xa.....',
       networkCheckTimeout:1000000000
     },
     test: {
@@ -74,7 +73,7 @@ module.exports = {
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
-      from:'0xA899F3a037Aae7Ab9601b92c7a6aBD9B22ee33b1',
+      from:'0xa....',
       networkCheckTimeout:1000000000
     },
     // Useful for private networks
